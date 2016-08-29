c6220fd81723d0ff463b4f86c432a8f972eb67fb
Dhanalakshmi-Sundaravel <dlakshmisdl@gmail.com>
2016-08-29 11:47:51 +0530

    Finding unique value in an array

---
+++ Unique.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Unique {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int count=0;
+		int[] regno=new int[n];
+		for(int i=0;i<n;i++)
+			regno[i]=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			count=0;
+			for(int j=0;j<n;j++)
+			{	
+				if(regno[i]==regno[j]&&j!=i)
+				{
+					count=1;
+					
+				}
+			}
+			if(count!=1)
+			System.out.println(regno[i]);
+			
+		}
+		sc.close();
+	}
+
+}
