d42ca5f1f49cb215b51dbebd4ed7f62fab86c80a
Dhanalakshmi-Sundaravel <dlakshmisdl@gmail.com>
2016-08-29 13:15:31 +0530

    To reverse the order of words in a string

---
+++ RevLine.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class RevLine {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		reverse(str);
+		sc.close();
+		}
+
+	
+	public static void reverse(String str)
+	{
+	String[] arr=str.split(" ");
+	for (int i = arr.length-1; i >=0 ; i--) {
+		System.out.print(arr[i]+" ");
+		
+	}
+	}
+}
+
